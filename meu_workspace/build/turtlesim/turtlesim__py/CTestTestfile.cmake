9b5107b4bc53ac5e4a2fc4bb7f03fc30507044ba
murilove <murilo.silva@sou.inteli.edu.br>
2024-05-02 17:46:31 -0300

    nada

---
+++ meu_workspace/build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/murilo_prianti/meu_workspace/build/turtlesim/turtlesim__py
+# Build directory: /home/murilo_prianti/meu_workspace/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
